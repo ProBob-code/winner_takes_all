689dbb0bfe733ab903ee391532a95b6bfb86ff31
probob <probob.in.git@gmail.com>
2026-04-20 15:30:21 +0530

    fix: specific edge and experimental-edge runtimes for fallback pages

---
+++ apps/web/pages/_error.tsx
@@ -0,0 +1,12 @@
+import NextError from 'next/error';
+
+export const runtime = 'experimental-edge';
+
+export default function CustomError({ statusCode }: { statusCode: number }) {
+  return <NextError statusCode={statusCode} />;
+}
+
+CustomError.getInitialProps = ({ res, err }: any) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
